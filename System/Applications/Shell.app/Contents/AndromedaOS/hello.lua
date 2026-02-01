e116738a5d1c9edd3ad29fdf25e910b40c8c8667
tocat <tocatnove@gmail.com>
2026-01-31 23:50:16 -0500

    Add verbose option to Logger via kernel arguments in Configuration

--- System/Applications/Shell.app/Contents/AndromedaOS/hello.lua
+++ System/Applications/Shell.app/Contents/AndromedaOS/hello.lua
@@ -1,13 +1,17 @@
 local currentdir = "/"
 local Config = require '/System/Libraries/Darwin/config'
-term.clear()
-term.setCursorPos(1, 1)
+if not (string.find(Config:FetchValueFromKey("KernelArgs"), "-v")) then
+    term.clear()
+    term.setCursorPos(1, 1)
+end
+
 print(os.version())
 function truncate(str)
     function findLast(haystack, needle)
         local i = haystack:match(".*" .. needle .. "()")
         if i == nil then return nil else return i - 1 end
     end
+
     local index = findLast(str, "/")
     return string.sub(str, 1, index)
 end
